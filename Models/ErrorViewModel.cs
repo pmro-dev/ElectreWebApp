c35a8205630b912c7d2735a10d07e16e782842bf
Piotr Markowski <markowskipriv@gmail.com>
2022-05-12 15:58:05 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElectreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
